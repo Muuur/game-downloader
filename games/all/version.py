4fe81539290af5846d29884fbdd5f797383b94f9
Muuur <46848501+Muuur@users.noreply.github.com>
2023-09-12 17:32:44 +0200

    Update version to script and PSP

--- games/all/version.py
+++ games/all/version.py
@@ -4,12 +4,12 @@ from typing import Final
 """
 Module containing all versions from current module
 """
-SCRIPT: Final[Version] = Version(1, 1, 0)
+SCRIPT: Final[Version] = Version(1, 2, 0)
 
 GB:   Final[Version] = Version(1, 0, 0)
 GBA:  Final[Version] = Version(1, 0, 0)
 GBC:  Final[Version] = Version(1, 0, 0)
-GCN:  Final[Version] = Version(1, 0, 0)
+GCN:  Final[Version] = Version(2, 0, 0)
 GEN:  Final[Version] = Version(1, 0, 0)
 GG:   Final[Version] = Version(1, 0, 0)
 N3DS: Final[Version] = Version(2, 0, 0)
@@ -19,10 +19,11 @@ NES:  Final[Version] = Version(1, 0, 0)
 PS2:  Final[Version] = Version(1, 0, 0)
 PS3:  Final[Version] = Version(1, 0, 0)
 PSX:  Final[Version] = Version(2, 0, 0)
+PSP:  Final[Version] = Version(1, 0, 0)
 SMD:  Final[Version] = GEN
 SMS:  Final[Version] = Version(1, 0, 0)
 SNES: Final[Version] = Version(1, 0, 0)
 WII:  Final[Version] = Version(1, 0, 0)
 WIIU: Final[Version] = Version(1, 0, 0)
 
-__all__ = ["SCRIPT", "WII", "N3DS", "PS2", "PSX", "GBA", "WIIU", "NES", "NDS", "SNES", "GBC", "GB", "N64", "GCN", "SMS", "SMD", "GG", "GEN"]
+__all__ = ["SCRIPT", "WII", "N3DS", "PS2", "PSX", "PSP", "GBA", "WIIU", "NES", "NDS", "SNES", "GBC", "GB", "N64", "GCN", "SMS", "SMD", "GG", "GEN"]
